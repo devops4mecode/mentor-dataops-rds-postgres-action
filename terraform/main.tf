765cd4eeec9ae133c9d423f9dfff2e917985b8f0
Najib Radzuan <najibradzuan@devops4me.com>
2023-04-14 12:27:41 +0800

    dev workflow updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,3 +29,19 @@ resource "aws_subnet" "subnet_b" {
     Name = "subnet_b"
   }
 }
+### Internet Gateway
+resource "aws_internet_gateway" "gateway" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gateway.id
+}
+
+resource "aws_eip" "gateway" {
+  count      = 2
+  vpc        = true
+  depends_on = [aws_internet_gateway.gateway]
+}
