31e030709eda12ee8f424912aae07f8fd98474b7
Najib Radzuan <najibradzuan@devops4me.com>
2023-04-14 10:46:16 +0800

    update workflow for terraform

--- terraform/backend/backend.tf
+++ terraform/backend/backend.tf
@@ -1,22 +1,8 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "do4m-dataops-tf"
-  force_destroy= true
-  versioning {
-    enabled = true
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock" {
-  name           = "do4m-dataops-tbl"
-  read_capacity  = 10
-  write_capacity = 10
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket         = "do4m-tf-main-bucket"
+    dynamodb_table = "do4m-tf-main-tbl"
+    key            = "LockID"
+    region         = "ap-southeast-1"
   }
 }
